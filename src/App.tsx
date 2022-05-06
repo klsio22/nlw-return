3d771bee92bf2bfa60b4ce3302ec245aa694535f
Klesio <klesio.dev@gmail.com>
2022-05-06 03:08:28 -0300

    New modifieds in app page

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,5 @@
-interface ButtonProps {
-  text?: string ;
-}
+import { Widget } from './components/Widget';
 
-function Button(props: ButtonProps) {
-  return <button className="bg-violet-500 px-4 h-10">{props.text ?? 'Default'}</button>;
+export function App() {
+  return <Widget />;
 }
-
-function App() {
-  return (
-    <div>
-      <Button text='Enviar' />
-      <Button text='ok' />
-      <Button  />
-    </div>
-  );
-}
-
-export default App;
