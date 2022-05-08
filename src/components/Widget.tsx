4948de2f4753d2b0c38478f1d587787a6eb97494
Klesio <klesio.dev@gmail.com>
2022-05-08 16:08:30 -0300

    Added accessibility in the widget

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,28 +1,18 @@
 import { ChatTeardropDots } from 'phosphor-react';
-import { useState } from 'react';
 import { Popover } from '@headlessui/react';
 
 export function Widget() {
-  const [isWidgetOpen, setIsWidgetOpen] = useState(false);
-
-  function toggleWidgetVisibility() {
-    setIsWidgetOpen(!isWidgetOpen);
-  }
-
   return (
-    <div className='absolute bottom-4 right-4'>
-      {isWidgetOpen && <p>Hello world</p>}
+    <Popover className='absolute bottom-4 right-4'>
+      <Popover.Panel>Hello world</Popover.Panel>
 
-      <button
-        onClick={toggleWidgetVisibility}
-        className='bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group'
-      >
+      <Popover.Button className='bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group'>
         <ChatTeardropDots className='w-6 h-6' />
 
         <span className='max-w-0 overflow-hidden group-hover:max-w-sm transition-all duration-500 ease-linear'>
           <span className='pl-2'>Feedback</span>
         </span>
-      </button>
-    </div>
+      </Popover.Button>
+    </Popover>
   );
 }
