eb59e850dc059fabd58c09b08bde34430860fe5c
Klesio <klesio.dev@gmail.com>
2022-05-08 16:04:14 -0300

    Added useState in a widget

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,9 +1,28 @@
 import { ChatTeardropDots } from 'phosphor-react';
+import { useState } from 'react';
+import { Popover } from '@headlessui/react';
 
 export function Widget() {
+  const [isWidgetOpen, setIsWidgetOpen] = useState(false);
+
+  function toggleWidgetVisibility() {
+    setIsWidgetOpen(!isWidgetOpen);
+  }
+
   return (
-    <button className='bg-violet-500 rounded-full px-3 h-12'>
-      <ChatTeardropDots className='w-6 h-6' />
-    </button>
+    <div className='absolute bottom-4 right-4'>
+      {isWidgetOpen && <p>Hello world</p>}
+
+      <button
+        onClick={toggleWidgetVisibility}
+        className='bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group'
+      >
+        <ChatTeardropDots className='w-6 h-6' />
+
+        <span className='max-w-0 overflow-hidden group-hover:max-w-sm transition-all duration-500 ease-linear'>
+          <span className='pl-2'>Feedback</span>
+        </span>
+      </button>
+    </div>
   );
 }
