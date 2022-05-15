c2c058ca589221c03b40901aea4a962775fc8b8b
Klesio Nascimento <klesio.dev@gmail.com>
2022-05-15 17:45:25 -0300

    applied the usestate and Keyof Type Operator

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState } from 'react';
 import { CloseButton } from './CloseButton';
 import bugImageUrl from '../assets/bug.svg';
 import ideaImageUrl from '../assets/idea.svg';
@@ -30,7 +30,11 @@ const feedbackTypes = {
   },
 };
 
+type FeedbackType = keyof typeof feedbackTypes;
+
 export default function WidgetForm() {
+  const [feedbackType, setFeedbackTypes] = useState<FeedbackType | null>(null);
+
   return (
     <div
       className='bg-zinc-900 p-4 relative rounded-2xl mb-4 flex 
@@ -42,22 +46,26 @@ export default function WidgetForm() {
         <CloseButton />
       </header>
 
-      <div className='flex py-8 gap-2 w-full'>
-        {Object.entries(feedbackTypes).map(([key, value]) => {
-
-          return (
-            <button
-              key={key}
-              className='bg-slate-800 rounded-lg py-5 w-24 flex-1 flex flex-col
+      {!feedbackType ? (
+        <div className='flex py-8 gap-2 w-full'>
+          {Object.entries(feedbackTypes).map(([key, value]) => {
+            return (
+              <button
+                key={key}
+                className='bg-slate-800 rounded-lg py-5 w-24 flex-1 flex flex-col
               items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none'
-              type='button'
-            >
-              <img src={value.image.source} alt={value.image.alt} />
-              <span>{value.title}</span>
-            </button>
-          );
-        })}
-      </div>
+                onClick={() => setFeedbackTypes(key as FeedbackType)}
+                type='button'
+              >
+                <img src={value.image.source} alt={value.image.alt} />
+                <span>{value.title}</span>
+              </button>
+            );
+          })}
+        </div>
+      ) : (
+        <p>Hello world</p>
+      )}
 
       <footer className='text-xs text-neutral-400 '>
         Feito com ♥ pela{' '}
