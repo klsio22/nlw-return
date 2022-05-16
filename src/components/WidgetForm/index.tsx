c61cd23fdc3252102bf2d41ce069ff0710dd5587
Klesio Nascimento <klesio.dev@gmail.com>
2022-05-15 23:20:52 -0300

    Added new proprietaries in feedbacks

---
+++ src/components/WidgetForm/index.tsx
@@ -0,0 +1,58 @@
+import { useState } from 'react';
+import { FeedbackContentStep } from './steps/FeedbackContentStep';
+import { FeedbackTypeStep } from './steps/FeedbackTypeStep';
+import bugImageUrl from '/src/assets/bug.svg';
+import ideaImageUrl from '/src/assets/idea.svg';
+import thoughtImageUrl from '/src/assets/thought.svg';
+
+export const feedbackTypes = {
+  BUG: {
+    title: 'Problema',
+    image: {
+      source: bugImageUrl,
+      alt: 'Imagem de um inseto',
+    },
+  },
+
+  IDEA: {
+    title: 'Ideia',
+    image: {
+      source: ideaImageUrl,
+      alt: 'Imagem de uma lâmpada',
+    },
+  },
+
+  OTHER: {
+    title: 'Outro',
+    image: {
+      source: thoughtImageUrl,
+      alt: 'Imagem de um balão de pensamento',
+    },
+  },
+};
+
+export type FeedbackType = keyof typeof feedbackTypes;
+
+export default function WidgetForm() {
+  const [feedbackType, setFeedbackTypes] = useState<FeedbackType | null>(null);
+
+  return (
+    <div
+      className='bg-zinc-900 p-4 relative rounded-2xl mb-4 flex 
+      flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto'
+    >
+      {!feedbackType ? (
+        <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackTypes} />
+      ) : (
+        <FeedbackContentStep feedbackType={feedbackType} />
+      )}
+
+      <footer className='text-xs text-neutral-400 '>
+        Feito com ♥ pela{' '}
+        <a href='' className='underline underline-offset-1'>
+          Rocketseat
+        </a>
+      </footer>
+    </div>
+  );
+}
